080cd2450fdd9add44a27e1108b3d015fdbed2a1
Justin.Saintil <justinfsaintil@verizon.net>
2014-04-23 09:50:08 -0400

    Fixed With New Addition

--- Dice.py
+++ Dice.py
@@ -2,15 +2,16 @@
 
 import random
 
-NumberOfDice = raw_input("How many dice")
+NumberOfDice = raw_input("How many dice?")
 NumberOfDice = int(NumberOfDice)
 
-NumberOfSides = raw_input("How many sides to your dice?")
-NumberOfSides = int(NumberOfSides)
+for x in range(0, NumberOfDice):
+	NumberOfSides= raw_input("How many sides does this one have?")
+	
 
+NumberOfSides= int(NumberOfSides)
 dice = random.randint(1,NumberOfSides)
 
-for Arthur in range(0,NumberOfDice):
+for x in range(0,NumberOfDice):
     print "The Result is", random.randint(0,NumberOfSides)
     dice = random.randint(1,NumberOfSides)
-
